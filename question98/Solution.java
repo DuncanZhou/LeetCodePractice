641c77e89d1d67439935ef38a6733d2558f7b33b
DuncanZhou <364451395@qq.com>
2017-11-14 18:26:43 +0800

    判断是否是二分搜索树

---
+++ question98/Solution.java
@@ -0,0 +1,36 @@
+package question98;
+
+import java.util.Stack;
+
+/**
+ * Created by duncan on 17-11-14.
+ */
+
+
+//判断是不是二分搜索树
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
+//递归判断,最后结果为左&右
+public class Solution {
+    public boolean inorder(TreeNode root, Stack<Integer> stack){
+        //中序遍历,left->root->right
+        boolean left = true,right = true;
+        if(root != null){
+            left = inorder(root.left,stack);
+            if(!stack.isEmpty() && root.val <= stack.peek())
+                return false;
+            else
+                stack.push(root.val);
+            right = inorder(root.right,stack);
+        }
+        return left && right;
+    }
+    public boolean isValidBST(TreeNode root) {
+        Stack<Integer> stack = new Stack<>();
+        return inorder(root,stack);
+    }
+}
