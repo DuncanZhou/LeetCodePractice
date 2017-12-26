4135f08fd25cff74c2c8f9c6f3ad9c33e9e8e994
DuncanZhou <364451395@qq.com>
2017-12-26 16:33:51 +0800

    最大连续和

--- question134/Solution.java
+++ question134/Solution.java
@@ -4,26 +4,39 @@ package question134;
  * Created by duncan on 17-12-26.
  */
 public class Solution {
+    //如果一个数组的总和非负，那么一定可以找到一个起始位置，从他开始绕数组一圈，累加和一直都是非负的
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int stations = gas.length;
-        for(int start = 0; start < stations; start++){
-            //选取不同的起始点
-            int leave = 0,index = start;
-            if(gas[index] < cost[index])
-                continue;
-            leave += gas[index] - cost[index];
-            index = (index+stations+1) % stations;
-            //从start开始
-            while((index+stations) % stations != start){
-                if(leave + gas[index] < cost[index])
-                    break;
-                leave += gas[index] - cost[index];
-                index = (index+stations+1) % stations;
+        int debt = 0, remain = 0,start = 0;
+        for(int i = 0; i < gas.length; i++){
+            remain += gas[i] - cost[i];
+            if(remain < 0)
+            {
+                debt += remain;
+                start = i + 1;
+                remain = 0;
             }
-            if((index+stations) % stations == start)
-                return start;
+
         }
-        return -1;
+        return remain + debt >= 0 ? start : -1;
+//        int stations = gas.length;
+//        for(int start = 0; start < stations; start++){
+//            //选取不同的起始点
+//            int leave = 0,index = start;
+//            if(gas[index] < cost[index])
+//                continue;
+//            leave += gas[index] - cost[index];
+//            index = (index+stations+1) % stations;
+//            //从start开始
+//            while((index+stations) % stations != start){
+//                if(leave + gas[index] < cost[index])
+//                    break;
+//                leave += gas[index] - cost[index];
+//                index = (index+stations+1) % stations;
+//            }
+//            if((index+stations) % stations == start)
+//                return start;
+//        }
+//        return -1;
     }
 
     public static void main(String[] args) {
