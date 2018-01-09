9c3332e95b6c5f08ae94ce074c94052f3013ee6c
DuncanZhou <364451395@qq.com>
2018-01-09 19:12:18 +0800

    旋转数组中包含重复元素

---
+++ question154/Solution.java
@@ -0,0 +1,16 @@
+package question154;
+
+/**
+ * Created by duncan on 18-1-9.
+ */
+public class Solution {
+    public int findMin(int[] nums) {
+        int res = nums[0];
+        if(nums.length == 1)
+            return res;
+        for(int i = 0; i < nums.length - 1; i++)
+            if(nums[i] > nums[i+1])
+                return nums[i+1];
+        return res;
+    }
+}
