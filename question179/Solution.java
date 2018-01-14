988e14792c0d14e7fcbc49162b43a49af6f52f61
DuncanZhou <364451395@qq.com>
2018-01-14 21:49:44 +0800

    拼接数字组成最大的数

---
+++ question179/Solution.java
@@ -0,0 +1,46 @@
+package question179;
+
+/**
+ * Created by duncan on 18-1-14.
+ */
+public class Solution {
+    public int compare(int a, int b){
+        String num1 = String.valueOf(a);
+        String num2 = String.valueOf(b);
+        String res1 = num1 + num2;
+        String res2 = num2 + num1;
+        for(int i = 0; i < res1.length(); i++){
+            if(res1.charAt(i) > res2.charAt(i))
+                return 1;
+            else if(res1.charAt(i) < res2.charAt(i))
+                return -1;
+        }
+        return 0;
+    }
+    public String largestNumber(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int j = 0; j < nums.length -1 - i;j++){
+                if(compare(nums[j],nums[j+1]) < 0)
+                {
+                    int swap = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = swap;
+                }
+            }
+        }
+        String res = "";
+        for(int i = 0; i < nums.length;i++)
+            res += String.valueOf(nums[i]);
+        if(res.charAt(0) == '0')
+            return "0";
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution method = new Solution();
+        System.out.println(method.compare(12,121));
+        int [] nums = {824,938,1399,5607,6973,5703,9609,4398,8247};
+        System.out.println(method.largestNumber(nums));
+    }
+}
