5d3e476e568c0da392d494b79d721728601269bc
DuncanZhou <364451395@qq.com>
2018-01-17 20:07:18 +0800

    反转链表,不使用其他空间

---
+++ question206/Solution.java
@@ -0,0 +1,49 @@
+package question206;
+
+/**
+ * Created by duncan on 18-1-17.
+ */
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode fakenode = new ListNode(0);
+        fakenode.next = head;
+        ListNode pre = fakenode,p = head,pp = p.next;
+        while(p != null){
+            if(pre != fakenode)
+                p.next = pre;
+            else
+                p.next = null;
+            pre = p;
+            p = pp;
+            if(p != null)
+                pp = p.next;
+        }
+        fakenode.next = pre;
+        return fakenode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+        Solution method = new Solution();
+        ListNode p = method.reverseList(node1);
+
+        while(p != null)
+        {
+            System.out.print(p.val + " ");
+            p = p.next;
+        }
+    }
+}
