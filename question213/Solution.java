690c37ec17ce704cc1aa488bfb077d5f757abaa3
DuncanZhou <364451395@qq.com>
2018-01-20 16:21:41 +0800

    动态规划简化版

---
+++ question213/Solution.java
@@ -0,0 +1,22 @@
+package question213;
+
+/**
+ * Created by duncan on 18-1-20.
+ */
+public class Solution {
+    //关于第一个抢还是不抢
+    private int rob(int[] num, int lo, int hi) {
+        int include = 0, exclude = 0;
+        for (int j = lo; j <= hi; j++) {
+            int i = include, e = exclude;
+            include = e + num[j];
+            exclude = Math.max(e, i);
+        }
+        return Math.max(include, exclude);
+    }
+    //所有的房子形成一个环
+    public int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+}
