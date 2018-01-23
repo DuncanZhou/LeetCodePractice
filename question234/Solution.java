149ce76a5069a45d0f03eb5b511559173f639160
DuncanZhou <364451395@qq.com>
2018-01-23 20:18:48 +0800

    判断链表是否是回文的,O(1)的空间复杂度,O(n)的时间复杂度

---
+++ question234/Solution.java
@@ -0,0 +1,47 @@
+package question234;
+
+/**
+ * Created by duncan on 18-1-23.
+ */
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+public class Solution {
+    //将后半段倒置,然后判断
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) { // odd nodes: let right half smaller
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+
+    }
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
