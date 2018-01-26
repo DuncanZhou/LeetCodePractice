2754fad0fbfda16b34553df21eda523bc1296eeb
DuncanZhou <364451395@qq.com>
2018-01-26 22:07:33 +0800

    找出排序好的数组中,各段连续的数值范围

---
+++ question228/Solution.java
@@ -0,0 +1,47 @@
+package question228;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by duncan on 18-1-26.
+ */
+public class Solution {
+    //找出排好序的数组中的范围
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if(nums == null || nums.length == 0)
+            return res;
+        int left = nums[0],right = nums[0];
+        int i = 1;
+        while (i < nums.length)
+        {
+            while(i < nums.length && nums[i] == nums[i-1])
+                i++;
+            if(i == nums.length)
+                break;
+            if(nums[i] != nums[i-1] + 1){
+                if(right == left)
+                    res.add(String.valueOf(left));
+                else
+                    res.add(String.valueOf(left)+"->"+String.valueOf(right));
+                if(i <= nums.length - 1) {
+                    left = nums[i ++];
+                    right = left;
+                }else
+                    break;
+            }else
+                right = nums[i++];
+        }
+        if(left != right)
+            res.add(String.valueOf(left)+"->"+String.valueOf(right));
+        else res.add(String.valueOf(left));
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution method = new Solution();
+        int[] nums = {0,2,3,4,6,8,9};
+        System.out.println(method.summaryRanges(nums));
+    }
+}
