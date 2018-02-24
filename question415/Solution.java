17e3113e6f860c436853c52c03bf9bb620b71c3f
DuncanZhou <364451395@qq.com>
2018-02-24 14:22:30 +0800

    两个字符数字相加

---
+++ question415/Solution.java
@@ -0,0 +1,45 @@
+package question415;
+
+/**
+ * Created by duncan on 18-2-24.
+ */
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int i = num1.length()-1, j = num2.length()-1, flag = 0;
+        while(i >= 0 && j >= 0){
+            int temp = num1.charAt(i)- '0' + num2.charAt(j)-'0' + flag;
+            if(temp >= 10)
+            {
+                flag = 1;
+                res.append(String.valueOf(temp -10));
+            }else{
+                flag = 0;
+                res.append(String.valueOf(temp));
+            }
+            i--;
+            j--;
+        }
+        int k = i == -1 ? j : i;
+        String num = i == -1 ? num2 : num1;
+        while(k >= 0){
+            int temp = num.charAt(k) -'0' + flag;
+            if(temp >= 10){
+                flag = 1;
+                res.append(String.valueOf(temp-10));
+            }else{
+                flag = 0;
+                res.append(String.valueOf(temp));
+            }
+            k--;
+        }
+        if(flag == 1)
+            res.append('1');
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Solution method = new Solution();
+        System.out.println(method.addStrings("123","87"));
+    }
+}
