a6f2c65b05f1560a617bd40ac14ba217a6e3311e
DuncanZhou <364451395@qq.com>
2018-03-12 14:00:46 +0800

    dfs搜索

---
+++ question399/Solution.java
@@ -0,0 +1,60 @@
+package question399;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Created by duncan on 18-3-12.
+ */
+public class Solution {
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        //邻接表存储图
+        HashMap<String, ArrayList<String>> pairs = new HashMap<String, ArrayList<String>>();
+        HashMap<String, ArrayList<Double>> valuesPair = new HashMap<String, ArrayList<Double>>();
+        for (int i = 0; i < equations.length; i++) {
+            String[] equation = equations[i];
+            if (!pairs.containsKey(equation[0])) {
+                pairs.put(equation[0], new ArrayList<String>());
+                valuesPair.put(equation[0], new ArrayList<Double>());
+            }
+            if (!pairs.containsKey(equation[1])) {
+                pairs.put(equation[1], new ArrayList<String>());
+                valuesPair.put(equation[1], new ArrayList<Double>());
+            }
+            pairs.get(equation[0]).add(equation[1]);
+            pairs.get(equation[1]).add(equation[0]);
+            //倒数也加进去
+            valuesPair.get(equation[0]).add(values[i]);
+            valuesPair.get(equation[1]).add(1/values[i]);
+        }
+
+        double[] result = new double[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String[] query = queries[i];
+            result[i] = dfs(query[0], query[1], pairs, valuesPair, new HashSet<String>(), 1.0);
+            if (result[i] == 0.0) result[i] = -1.0;
+        }
+        return result;
+    }
+    //深度搜索图
+    private double dfs(String start, String end, HashMap<String, ArrayList<String>> pairs, HashMap<String, ArrayList<Double>> values, HashSet<String> set, double value) {
+        if (set.contains(start)) return 0.0;
+        if (!pairs.containsKey(start)) return 0.0;
+        if (start.equals(end)) return value;
+        //遍历过
+        set.add(start);
+
+        ArrayList<String> strList = pairs.get(start);
+        ArrayList<Double> valueList = values.get(start);
+        double tmp = 0.0;
+        for (int i = 0; i < strList.size(); i++) {
+            tmp = dfs(strList.get(i), end, pairs, values, set, value*valueList.get(i));
+            if (tmp != 0.0) {
+                break;
+            }
+        }
+        set.remove(start);
+        return tmp;
+    }
+}
