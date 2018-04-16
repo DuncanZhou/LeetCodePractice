b2f4c52b73bcb741339f783614e1c8a3ee678d61
DuncanZhou <364451395@qq.com>
2018-04-16 15:29:13 +0800

    dfs搜索,也是一道经典题目

---
+++ question698/Solution.java
@@ -0,0 +1,44 @@
+package question698;
+
+import java.util.Arrays;
+
+/**
+ * Created by duncan on 18-4-16.
+ */
+public class Solution {
+    //判断是否能将数组分为k个和相等的子数组
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int[] res = new int[k];
+        int sum = 0;
+        for(int x : nums)
+            sum += x;
+        if(sum % k != 0) return false;
+        Arrays.sort(nums);
+        int target = sum / k;
+        if(nums[nums.length - 1] > target) return false;
+        return helper(nums,res,0,target);
+    }
+    private boolean helper(int[] nums,int[] res,int index,int target){
+        if(index == nums.length){
+            boolean flag = true;
+            for(int i = 0; i < res.length; i++)
+                if(res[i] != target){flag = false; break;}
+            return flag;
+        }else{
+            for(int i = 0; i < res.length; i++){
+                if(res[i] == target) continue;
+                else if(res[i] + nums[index] > target) return false;
+                res[i] += nums[index];
+                if(helper(nums,res,index+1,target)) return true;
+                res[i] -= nums[index];
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution method = new Solution();
+        int[] nums = {724,3908,1444,522,325,322,1037,5508,1112,724,424,2017,1227,6655,5576,543};
+        System.out.println(method.canPartitionKSubsets(nums,4));
+    }
+}
