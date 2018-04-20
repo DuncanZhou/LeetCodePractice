d8cb6e3dac14da0b8166d771832be571dab8cb6d
DuncanZhou <364451395@qq.com>
2018-04-20 16:00:21 +0800

    一个数组中所有数出现了k次,只有一个数出现了一次,找出这个数

--- question137/Solution.java
+++ question137/Solution.java
@@ -1,41 +1,23 @@
 package question137;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by duncan on 17-12-27.
  */
 public class Solution {
     //数组中每个数都出现了3次,除了一个数,找到那个数
     public int singleNumber(int[] nums) {
-        if (nums == null || nums.length == 0)
-            return -1;
-        if (nums != null && nums.length == 1)
-            return nums[0];
-        Map<Integer, Integer> maps = new HashMap<Integer, Integer>();
-        List<Integer> filter = new ArrayList<Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                maps.put(nums[i], 1);
-            } else {
-                if (!filter.contains(nums[i])) {
-                    if (maps.get(nums[i]) == null) {
-                        maps.put(nums[i], 1);
-                    } else {
-                        maps.put(nums[i], maps.get(nums[i]) + 1);
-                    }
-
-                    if (maps.get(nums[i]) > 1) {
-                        maps.remove(nums[i]);
-                        filter.add(nums[i]);
-                    }
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            int sum = 0;
+            for(int x:nums){
+                if(((x>>i)&1) == 1){
+                    sum ++;
+                    sum %= 3;
                 }
             }
+            if(sum != 0)
+                res |= sum << i;
         }
-        return maps.keySet().toArray(new Integer[0])[0];
+        return res;
     }
 }
